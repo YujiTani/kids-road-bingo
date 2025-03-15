f4437d9c6f15611a1043e00676c865e1a481a44c
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-03-15 09:46:42 +0900

    バレルに画像を追加

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -18,6 +18,27 @@ import CarKoujiShovelcarPng from './car_kouji_shovelcar.png'
 import CarKoujiCranePng from './car_kouji_crane.png'
 import CarKoujiRoadRollerPng from './car_kouji_road_roller.png'
 import CarTankerTruckPng from './car_tanker_truck.png'
+// == ここから
+import CarMixerTruckPng from './car_mixer_truck.png'
+import TrainBluePng from './Train Blue.png'
+import ShipPng from './ship.png'
+import AnimalDogSanpoPng from './animal_dog_sanpo.png'
+import AnimalCatMikePng from './animal_cat_mike.png'
+import LogoMcdibaldoPng from './logo_mcdibaldo.png'
+import WeatherRainbowPng from './weather_rainbow.png'
+import WeatherKaminariPng from './weather_kaminari.png'
+import ObjectTunnelPng from './object_tunnel.png'
+import ObjectToriiPng from './object_torii.png'
+import ObjectTettouPng from './object_tettou.png'
+import ObjectTeraPng from './object_tera.png'
+import ObjectKouenPng from './object_kouen.png'
+import ObjectChosuitouPng from './object_chosuitou.png'
+import ObjectByouinPng from './object_byouin.png'
+import ObjectGasTankPng from './object_gas_tank.png'
+import ObjectCircusTentPng from './object_circus_tent.png'
+import ObjectFoodHamburgerPng from './object_food_hamburger.png'
+import ObjectFoodPizzaPng from './object_food_pizza.png'
+
 
 export { 
     DriveBingoOgpPng as DriveBingoOgp,
@@ -40,4 +61,23 @@ export {
     CarKoujiCranePng as CarKoujiCrane,
     CarKoujiRoadRollerPng as CarKoujiRoadRoller,
     CarTankerTruckPng as CarTankerTruck,
+    CarMixerTruckPng as CarMixerTruck,
+    TrainBluePng as TrainBlue,
+    ShipPng as Ship,
+    AnimalDogSanpoPng as AnimalDogSanpo,
+    AnimalCatMikePng as AnimalCatMike,
+    LogoMcdibaldoPng as LogoMcdibaldo,
+    WeatherRainbowPng as WeatherRainbow,
+    WeatherKaminariPng as WeatherKaminari,
+    ObjectTunnelPng as ObjectTunnel,
+    ObjectToriiPng as ObjectTorii,
+    ObjectTettouPng as ObjectTettou,
+    ObjectTeraPng as ObjectTera,
+    ObjectKouenPng as ObjectKouen,
+    ObjectChosuitouPng as ObjectChosuitou,
+    ObjectByouinPng as ObjectByouin,
+    ObjectGasTankPng as ObjectGasTank,
+    ObjectCircusTentPng as ObjectCircusTent,
+    ObjectFoodHamburgerPng as ObjectFoodHamburger,
+    ObjectFoodPizzaPng as ObjectFoodPizza,
 };
